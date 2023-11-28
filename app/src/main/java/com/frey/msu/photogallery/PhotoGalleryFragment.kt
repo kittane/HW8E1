b777626771e56cefe217b2977c8a84ffaec2af1f
Zack <zpf5s@login.missouristate.edu>
2023-11-27 21:44:29 -0600

    Added basic fragment

---
+++ app/src/main/java/com/frey/msu/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,37 @@
+package com.frey.msu.photogallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.frey.msu.photogallery.databinding.FragmentPhotoGalleryBinding
+
+class PhotoGalleryFragment : Fragment() {
+
+    private var _binding: FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =
+            FragmentPhotoGalleryBinding.inflate(inflater, container, false)
+        binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
